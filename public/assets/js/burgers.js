02ca27424ed0023b13cc027de8ffac89b823a015
VinceH28 <VincentHart28@gmail.com>
2021-02-13 14:57:54 -0500

    update burgers.js
    Create 'burgerbtn'
    Send POST request to create a new quote.
    Serialize the JSON body.
    Empty the form
    Reload the page so the user can see the new quote

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -40,4 +40,57 @@ if (changeDevouredBtns) {
       });
     });
   }  
+    // CREATE
+    const createBurgerBtn = document.getElementById('create-form');
+
+    if (createBurgerBtn) {
+      createBurgerBtn.addEventListener('submit', (e) => {
+        e.preventDefault();
   
+        // Grabs the value of the textarea that goes by the name, "quote"
+        const newBurger = {
+          name: document.getElementById('bg').value.trim(),
+          devoured: document.getElementById('devoured').checked,
+        };
+  
+        // Send POST request to create a new quote
+        fetch('/api/burgers', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+  
+          // Serialize the JSON body
+          body: JSON.stringify(newBurger),
+        }).then(() => {
+          // Empty the form
+          document.getElementById('bg').value = '';
+  
+          // Reload the page so the user can see the new quote
+          console.log('Burger has been devoured!');
+          location.reload();
+        });
+      });
+    }
+    // DELETE
+    const deleteBurgerBtns = document.querySelectorAll('.delete-burger');
+  
+    // Set up the event listeners for each delete button
+    deleteBurgerBtns.forEach((button) => {
+      button.addEventListener('click', (e) => {
+        const id = e.target.getAttribute('data-id');
+  
+        // Send the delete request
+        fetch(`/api/burgers/${id}`, {
+          method: 'DELETE',
+        }).then((res) => {
+          console.log(res);
+          console.log(`Deleted burger: ${id}`);
+  
+          // Reload the page
+          location.reload();
+        });
+      });
+    });
+  });
