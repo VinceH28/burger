e453b108921802076f23406961729699acc02ace
VinceH28 <VincentHart28@gmail.com>
2021-02-19 11:21:06 -0500

    Update orm.js
    setup MySQL connection &  Helper function for SQL syntax to add question marks (?, ?, ?) in query

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+// Import MySQL connection.
+const connection = require('./connection.js');
+
+// Helper function for SQL syntax to add question marks (?, ?, ?) in query
+const printQuestionMarks = (num) => {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push('?');
+  }
+
+  return arr.toString();
+};
+
