2c5636aec7b9958feb32a00428eff66bcf3f3fb2
VinceH28 <VincentHart28@gmail.com>
2021-02-19 11:50:22 -0500

    Update seeds.sql
    updated file

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Cheeseburger');
+INSERT INTO burger (name) VALUES ('Avacado Bean Vegan Burger');
+INSERT INTO burger (name, devoured) VALUES ('Bacon Double Cheese Burger', true);
+INSERT INTO burger (name, devoured) VALUES ('Hamburger ', true);
+INSERT INTO burger (name, devoured) VALUES ('Steakhouse Burger', true);
+INSERT INTO burger (name) VALUES ('Ramen Burger');
