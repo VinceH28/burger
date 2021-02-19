2ff94546621c715c37fec9d74d18c4624ec8010c
VinceH28 <VincentHart28@gmail.com>
2021-02-19 11:24:54 -0500

    Update orm.js
    created object for all our SQL statement functions.
    &
    setup export of the orm object for the model

--- config/orm.js
+++ config/orm.js
@@ -32,3 +32,56 @@ const objToSql = (ob) => {
   return arr.toString();
 };
 
+// Object for all our SQL statement functions.
+const orm = {
+  all(tableInput, cb) {
+    const queryString = `SELECT * FROM ${tableInput};`;
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create(table, cols, vals, cb) {
+    let queryString = `INSERT INTO ${table}`;
+
+    queryString += ' (';
+    queryString += cols.toString();
+    queryString += ') ';
+    queryString += 'VALUES (';
+    queryString += printQuestionMarks(vals.length);
+    queryString += ') ';
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // An example of objColVals would be {name: bacon cheese burger, devoured: true}
+  update(table, objColVals, condition, cb) {
+    let queryString = `UPDATE ${table}`;
+
+    queryString += ' SET ';
+    queryString += objToSql(objColVals);
+    queryString += ' WHERE ';
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
