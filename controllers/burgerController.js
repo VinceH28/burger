603f68fd5a7efa4fa923e800088e36e5656f36df
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-10 19:37:14 -0400

    Update burgerController.js

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
   });
 
   router.post('/api/burgers', (req, res) => {
-    burger.create(['name', 'devoured'], [req.body.name, req.body.devoured], (result) => {
+    burger.create(['burger_name', 'devoured'], [req.body.name, req.body.devoured], (result) => {
       res.json({ id: result.insertId });
     });
   });
